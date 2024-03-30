70f04063160ec9ac1e2c007e3d66a08b62f3930d
Mark-Randria <randrianjafymark2.0@gmail.com>
2024-03-30 22:31:59 +0300

    Pour deplacer la methode toString() dans les sous-classes, if faut surcharger la methode toString()

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -4,5 +4,9 @@ public class Femme extends Humain {
 	Femme(String nom, String prenom, int age) {
 		super(nom, prenom, age);
 	}
-
+	
+	@Override
+	public String toString() {
+		return super.toString();
+	}
 }
--- src/iut/bad/Homme.java
+++ src/iut/bad/Homme.java
@@ -4,4 +4,9 @@ public class Homme extends Humain {
 	Homme(String nom, String prenom, int age) {
 		super(nom, prenom, age);
 	}
+	
+	@Override
+	public String toString() {
+		return super.toString();
+	}
 }
